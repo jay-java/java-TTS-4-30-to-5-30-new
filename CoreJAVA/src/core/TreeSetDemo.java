9f4d6eab96bb6c202e1eadd0afe91150529a93a3
jay-java <jayprajapati.tops@gmail.com>
2023-02-21 17:01:33 +0530

    collection

---
+++ CoreJAVA/src/core/TreeSetDemo.java
@@ -0,0 +1,16 @@
+package core;
+
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+	public static void main(String[] args) {
+		TreeSet set = new TreeSet();
+		set.add(12);
+		set.add(346);
+		set.add(456);
+		set.add(-984);
+		set.add(0);
+		System.out.println(set);
+		
+	}
+}
