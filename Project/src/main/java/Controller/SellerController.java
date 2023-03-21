4020274fb1c1d0e78e7c1d30d82f45c4c41910a7
jay-java <jayprajapati.tops@gmail.com>
2023-03-21 17:34:37 +0530

    project register login logout

---
+++ Project/src/main/java/Controller/SellerController.java
@@ -0,0 +1,69 @@
+package Controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import Dao.SellerDao;
+import Model.Seller;
+
+/**
+ * Servlet implementation class SellerController
+ */
+@WebServlet("/SellerController")
+public class SellerController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SellerController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String action = request.getParameter("action");
+		if(action.equalsIgnoreCase("register")) {
+			Seller s = new Seller();
+			s.setName(request.getParameter("name"));
+			s.setContact(Long.parseLong(request.getParameter("contact")));
+			s.setAddress(request.getParameter("address"));
+			s.setEmail(request.getParameter("email"));
+			s.setPassword(request.getParameter("password"));
+			SellerDao.insertSeller(s);
+			request.setAttribute("msg", "data registered successfully");
+			request.getRequestDispatcher("seller-login.jsp").forward(request, response);
+		}
+		else if(action.equalsIgnoreCase("login")) {
+			Seller s = new Seller();
+			s.setEmail(request.getParameter("email"));
+			s.setPassword(request.getParameter("password"));
+			Seller s1 = SellerDao.sellerLogin(s);
+			if(s1 == null) {
+				
+			}
+			else {
+				HttpSession session = request.getSession();
+				session.setAttribute("data", s1);
+				request.getRequestDispatcher("seller-home.jsp").forward(request, response);
+			}
+		}
+	}
+
+}
