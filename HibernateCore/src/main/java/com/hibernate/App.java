8e5e612feccf2342705e65719eff26891b796d12
jay-java <jayprajapati.tops@gmail.com>
2023-04-18 17:41:05 +0530

    hibernate intro

---
+++ HibernateCore/src/main/java/com/hibernate/App.java
@@ -0,0 +1,22 @@
+package com.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		SessionFactory sf = new Configuration().configure("com/hibernate/hibernate.cfg.xml").buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+		
+		UserData u = new UserData(3, "kotlin", 3216548);
+		session.delete(u);
+		
+		tx.commit();
+		session.close();
+		sf.close();
+	}
+}
